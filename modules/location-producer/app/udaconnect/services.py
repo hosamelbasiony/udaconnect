c5e3e6299b0f4e75eb06adf28c7b1b1a7ca18d95
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2022-12-06 15:47:43 +0200

    0000000000

--- modules/location-producer/app/udaconnect/services.py
+++ modules/location-producer/app/udaconnect/services.py
@@ -9,6 +9,7 @@ from app.udaconnect.models import Connection, Location, Person
 from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
+from json import dumps, loads
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-api")
@@ -116,7 +117,7 @@ class LocationService:
             "longitude": location["longitude"]
         }
         producer = g.kafka_producer
-        producer.send("location", location)
+        producer.send("location", loads(location))
         producer.flush()
 
         return new_location
