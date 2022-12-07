37c1fc77c7543428e3c6add278a5984abc6a8c84
hosamelbasiony <hosam.el.basiony@gmail.com>
2022-12-07 23:13:52 +0000

    gggggggggg

--- modules/location-consumer/app/udaconnect/consumer.py
+++ modules/location-consumer/app/udaconnect/consumer.py
@@ -12,14 +12,17 @@ messages = KafkaConsumer(TOPIC_NAME,bootstrap_servers=KAFKA_SERVER,api_version=(
 
 
 def insert_location(location: Dict):
-    session = psycopg2.connect(dbname=DB_NAME, port=DB_PORT, user=DB_USERNAME, password=DB_PASSWORD, host=DB_HOST)
-    cursor = session.cursor()
-    cursor.execute(
-        'INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}));'.format(
+    # session = psycopg2.connect(dbname=DB_NAME, port=DB_PORT, user=DB_USERNAME, password=DB_PASSWORD, host=DB_HOST)
+    # cursor = session.cursor()
+    # cursor.execute(
+    #     'INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}));'.format(
+    #         int(location["person_id"]), float(location["latitude"]), float(location["longitude"])))
+    # session.commit()
+    # cursor.close()
+    # session.close()
+
+    print('INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}));'.format(
             int(location["person_id"]), float(location["latitude"]), float(location["longitude"])))
-    session.commit()
-    cursor.close()
-    session.close()
 
     print("Location added to the database!")
     print(location)
