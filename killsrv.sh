5226c6f3a054b004529c45d0cf03408495d25385
hosamelbasiony <hosam.el.basiony@gmail.com>
2022-12-08 11:48:03 +0000

    gggggggggg

---
+++ killsrv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+kubectl delete service kubernetes 
+kubectl delete service kafka      
+kubectl delete service postgres    
+kubectl delete service udaconnect-kafka-producer
+kubectl delete service udaconnect-app           
+kubectl delete service kafka-zookeeper-headless 
+kubectl delete service udaconnect-kafka-consumer
+kubectl delete service udaconnect-notifications-service 
+kubectl delete service kafka-zookeeper                  
+kubectl delete service kafka-headless                   
+kubectl delete service udaconnect-api 
