2bba9e65e50a09e06b8efbb5ba9fbca85594566e
hosamelbasiony <hosam.el.basiony@gmail.com>
2022-12-09 22:43:07 +0000

    generic commit

--- modules/persons-api/wsgi.py
+++ modules/persons-api/wsgi.py
@@ -4,4 +4,4 @@ from app import create_app
 
 app = create_app(os.getenv("FLASK_ENV") or "test")
 if __name__ == "__main__":
-    app.run(debug=True, host=0.0.0.0)
+    app.run(debug=True, host="0.0.0.0")
