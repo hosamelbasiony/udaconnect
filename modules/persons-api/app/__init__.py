19792b5c2ca7f6c9e12d23e3098f791fdabfec35
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2022-12-11 19:44:32 +0200

    1111111

--- modules/persons-api/app/__init__.py
+++ modules/persons-api/app/__init__.py
@@ -3,8 +3,11 @@ from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+import logging
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
+db = SQLAlchemy()
 
 def create_app(env=None):
     from app.config import config_by_name
@@ -13,6 +16,24 @@ def create_app(env=None):
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
     api = Api(app, title="UdaConnect API", version="0.1.0")
+
+    ################################################################
+    ################################################################
+    ## TRACING #####################################################
+    ################################################################    
+    config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+                        
+    jaeger_tracer = config.initialize_tracer()
+    tracing = FlaskTracing(jaeger_tracer, True, app)
+    ################################################################
+    ################################################################
 
     CORS(app)  # Set CORS for development
 
